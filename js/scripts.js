6593e7f3f90e1b9cf77c3242bd3d9982257842f6
DiegoSerra <30292837+DiegoSerra@users.noreply.github.com>
2017-10-17 09:27:27 +0200

    validate email

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(function() {
 	$("#tabs").tabs(); 
+
+	function validateEmail(email) {
+      var expr = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      return expr.test(email);
+    }
+
+
+    $("#submit").click(function () {
+        if (!validateEmail($("#email").val())) {
+            alert("Introduzca un email valido");
+            return false;
+        }
+    });
 }); 
